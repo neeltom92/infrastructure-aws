3f9eb7298322473ffedd860800579286df9cd474
Neel Thomas <neelthomas@Neels-MacBook-Pro.local>
2022-08-28 23:15:49 +0530

    feat: added additional changes to git ignore version 7

--- vpc/prod/ap-southeast-1/provider.tf
+++ vpc/prod/ap-southeast-1/provider.tf
@@ -10,7 +10,8 @@ terraform {
     dynamodb_table = "dynamo-tf-lock"
   }
 }
-# this terraform is referenced from the below
-# https://faun.pub/terraform-with-aws-create-aws-vpc-with-private-public-subnet-with-nat-gateway-security-group-ebbe643b7a66
+# this terraform is referenced from the below https://faun.pub/terraform-with-aws-create-aws-vpc-with-private-public-subnet-with-nat-gateway-security-group-ebbe643b7a66
+# if needed mutiple NAT gw'S  use this as the reference : https://stackoverflow.com/questions/71087224/configure-multiple-nat-gateways
+
 
 
